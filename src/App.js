78efa4aa334cf8e4af1ffebf727610788788ef2d
junaet faruk rafi <rafee790@gmail.com>
2022-05-31 15:04:54 +0600

    Navigation bar using Reactstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import React from 'react';
 import './App.css';
-import './components/MainComponent';
+import MainComponent from './components/MainComponent';
+
 
 function App() {
   return (
     <div className="App">
-
+      <MainComponent />
     </div>
   );
 }
